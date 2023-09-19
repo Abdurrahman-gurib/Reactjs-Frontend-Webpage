2c3fea9fb3f76aaaf5f438ea3cd7fdfeeca17941
Abdurrahman Gurib (RAPP) <abdurrahman.gurib@rapp.com>
2023-09-19 17:40:47 +0400

    Initial Commits

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+// App.js
+import React from "react";
+import AppRouter from "./Router";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        {"This is a Beatifull FrontEnd, Not Really"}
+        <nav>
+          <ul>
+            <li>
+              <a href="/">Home</a>
+            </li>
+            <li>
+              <a href="/about">About</a>
+            </li>
+            <li>
+              <a href="/contact">Contact</a>
+            </li>
+          </ul>
+        </nav>
       </header>
+
+      {/* Render the Router component */}
+      <main>
+        <AppRouter />
+      </main>
     </div>
   );
 }
---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+// pages/Home.js
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <h2>Welcome to the Home Page</h2>
+      <p>This is the Home page content.</p>
+    </div>
+  );
+};
+
+export default Home;
